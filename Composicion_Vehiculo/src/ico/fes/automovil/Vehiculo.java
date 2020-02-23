7c30bfc41ef524825a9ab667f03d1c42fa011171
Ariok-David <60637430+Ariok-David@users.noreply.github.com>
2020-02-23 16:23:26 -0600

    Tarea #5 de POO

---
+++ Composicion_Vehiculo/src/ico/fes/automovil/Vehiculo.java
@@ -0,0 +1,83 @@
+package ico.fes.automovil;
+
+import ico.fes.componentes.Chasis;
+import ico.fes.componentes.Llanta;
+import ico.fes.componentes.Motor;
+
+public class Vehiculo {
+    private float precio;
+    private String color;
+    private String fabricante;
+    private Llanta neumatico;
+    private Chasis armazon;
+    private Motor maquina;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(float precio, String color, String fabricante, Llanta neumatico, Chasis armazon, Motor maquina) {
+        this.precio = precio;
+        this.color = color;
+        this.fabricante = fabricante;
+        this.neumatico = neumatico;
+        this.armazon = armazon;
+        this.maquina = maquina;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    
+    public void encender(){
+        System.out.println("Encendiendo auto......");
+    }
+    
+    public void apagar(){
+        System.out.println("Apagando auto..");
+    }
+
+    public Llanta getNeumatico() {
+        return neumatico;
+    }
+
+    public void setNeumatico(Llanta neumatico) {
+        this.neumatico = neumatico;
+    }
+
+    public Chasis getArmazon() {
+        return armazon;
+    }
+
+    public void setArmazon(Chasis armazon) {
+        this.armazon = armazon;
+    }
+
+    public Motor getMaquina() {
+        return maquina;
+    }
+
+    public void setMaquina(Motor maquina) {
+        this.maquina = maquina;
+    }
+    
+}
